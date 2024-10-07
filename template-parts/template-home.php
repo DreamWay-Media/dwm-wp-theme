6a231b14fcc8b1081efb26b9f3f7299efe145bf7
nishika-lewis <nishikalewis15@gmail.com>
2024-10-07 17:10:28 -0400

    Fixed EST 2009 scroll in template-home.php

--- template-parts/template-home.php
+++ template-parts/template-home.php
@@ -47,14 +47,14 @@ get_header();
   
   <!--scroll down-->
   <div class="scroll-down-btn" data-aos="fade-up">
-    <a href="#arrowsection"><img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/circle.svg'); ?>" alt="circle"></a>
+    <a href="#about-main-wrap"><img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/circle.svg'); ?>" alt="circle"></a>
   </div>
   <!--scroll down--> 
   
 </section>
 <!--header slider--> 
 
-<section class="about-main-wrap"> 
+<section class="about-main-wrap" id="about-main-wrap"> 
   <!--about-->
   <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
   <div class="container bootstrap snippets bootdey " data-aos="fade-up">
