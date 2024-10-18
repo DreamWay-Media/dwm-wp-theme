da2d00a781d1cfaa93e24fe49600616a05d458e0
PranjalSharma13 <1SI17CS077@sit.ac.in>
2024-10-17 23:30:35 -0500

    embedded script

--- functions.php
+++ functions.php
@@ -244,3 +244,9 @@ function has_submenu($menuitems, $parent_id) {
     }
     return false;
 }
+function add_accessibility_script() {
+    ?>
+    <script> (function(){ var s = document.createElement('script'); var h = document.querySelector('head') || document.body; s.src = 'https://acsbapp.com/apps/app/dist/js/app.js'; s.async = true; s.onload = function(){ acsbJS.init({ statementLink : '', footerHtml : '', hideMobile : false, hideTrigger : false, disableBgProcess : false, language : 'en', position : 'left', leadColor : '#000000', triggerColor : '#c8f031', triggerRadius : '50%', triggerPositionX : 'left', triggerPositionY : 'bottom', triggerIcon : 'people', triggerSize : 'medium', triggerOffsetX : 20, triggerOffsetY : 20, mobile : { triggerSize : 'small', triggerPositionX : 'right', triggerPositionY : 'bottom', triggerOffsetX : 10, triggerOffsetY : 10, triggerRadius : '50%' } }); }; h.appendChild(s); })(); </script>
+    <?php
+}
+add_action('wp_footer', 'add_accessibility_script');
