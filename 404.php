b9cb18f5d96fd5ec37dcb5d6d9a2e5fb2d5d5ece
Charles Milton <139507918+chuckmilton@users.noreply.github.com>
2024-10-31 13:29:53 -0700

    Remove extra content and add homepage button

--- 404.php
+++ 404.php
@@ -11,54 +11,20 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-
     <section class="error-404 not-found">
+        
         <header class="page-header">
-            <h1 class="page-title"><?php esc_html_e( 'Oops! We couldn\'t find that page.', 'dreamway-media' ); ?></h1>
-            <p class="page-subtitle"><?php esc_html_e( 'It looks like nothing was found at this location.', 'dreamway-media' ); ?></p>
+            <h1 class="page-title">Oops! We couldn't find that page.</h1>
+            <p>It looks like nothing was found at this location.</p>
         </header><!-- .page-header -->
 
         <div class="page-content">
             <p><?php esc_html_e( 'Maybe try one of the links below or a search?', 'dreamway-media' ); ?></p>
-
             <?php get_search_form(); ?>
-
-            <div class="widget widget_recent_posts">
-                <?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 5 ) ); ?>
-            </div>
-
-            <div class="widget widget_categories">
-                <h2 class="widgettitle"><?php esc_html_e( 'Most Used Categories', 'dreamway-media' ); ?></h2>
-                <ul>
-                    <?php
-                    wp_list_categories(
-                        array(
-                            'orderby'    => 'count',
-                            'order'      => 'DESC',
-                            'show_count' => 1,
-                            'title_li'   => '',
-                            'number'     => 10,
-                        )
-                    );
-                    ?>
-                </ul>
-            </div><!-- .widget -->
-
-            <?php
-            $dreamway_media_archive_content = sprintf(
-                '<p>' . esc_html__( 'Or check out the monthly archives %1$s', 'dreamway-media' ),
-                convert_smilies( ':)' ).'</p>'
-            );
-            the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$dreamway_media_archive_content" );
-            ?>
-
-            <div class="widget widget_tag_cloud">
-                <?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-            </div>
-
+            <a href="<?php echo esc_url(home_url('/')); ?>" class="button">Return to Homepage</a>
         </div><!-- .page-content -->
-    </section><!-- .error-404 -->
 
+    </section><!-- .error-404 -->
 </main><!-- #main -->
 
 <?php
