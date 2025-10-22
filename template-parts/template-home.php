051e9f303edbd9fbec4a5ccea65c611eef425352
vrej.sanati <vrej.sanati@munchkin.com>
2025-10-22 13:45:01 -0700

    Enhance heading styles in template-home.php for better visual impact. Updated margin, font size, and line height for the 'we_offer_heading' to improve layout consistency.

--- template-parts/template-home.php
+++ template-parts/template-home.php
@@ -91,7 +91,7 @@ get_header();
         <div class="container bootstrap snippets bootdey home-container">
           <div class="row text-center">
             <div class="col-12">
-              <h2><?php echo get_field('we_offer_heading'); ?></h2>
+              <h2 style="margin-bottom: 30px; font-size: 40px; line-height: 45px; font-weight: bold;"><?php echo get_field('we_offer_heading'); ?></h2>
               <h3 class="highlighted-heading"><?php echo get_field('we_offer_description_right'); ?></h3>
               
             </div>
